190814fd5445e9956d50a60309c930be554782f3
minpole <minpole727@gmail.com>
2025-08-24 00:12:20 +0900

    ch5.3 작성

---
+++ apis/seoul_data/realtime_bicycle.py
@@ -0,0 +1,9 @@
+class RealtimeBicycle:
+   
+    def __init__(self):
+        """RealtimeBicycle 클래스의 인스턴스를 초기화합니다."""
+        self.auth_key = "##auth_key_seoul_data##"
+
+    def call_api(self) -> None:
+        """서울시 자전거 실시간 API를 호출합니다."""
+        pass
